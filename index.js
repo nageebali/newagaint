4ce5dc28e928a161fda6a1fcccaa8e5a7c656191
nageebali <nageebali0@gmail.com>
2025-06-14 17:55:50 +0300

    Update index.jswsten2

--- index.js
+++ index.js
@@ -24,12 +24,7 @@ const logger = winston.createLogger({
 });
 
 
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' })
-  ]
-});
+
 
 const app = express();
 const server = createServer(app);
